d695f90773ec30fbe5d828bf45f725619da0f396
NonExistentPotato <93904995+NonExistentPotato@users.noreply.github.com>
2021-11-11 11:37:40 +0530

    Update display.py

--- display.py
+++ display.py
@@ -1,12 +1,21 @@
 import cv2
+import mediapipe as mp
+from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(0)
 cap.set(3,1280)
 cap.set(4,720)
+detector = HandDetector(detectionCon=0.8)
 
 while True:
     success, img = cap.read()
     img = cv2.flip(img, 1)
+
+    hands, img = detector.findHands(img, flipType=False)
+
     cv2.imshow("Display",img)
-    cv2.waitKey(10)
-    cv2.destroyAllWindows()
+    key = cv2.waitKey(10)
+    if key == ord('q'):
+        break
+
+cv2.destroyAllWindows()
