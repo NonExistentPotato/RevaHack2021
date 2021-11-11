e7c849fdf30e437e2e5dd0579e1191a0211ab014
NonExistentPotato <93904995+NonExistentPotato@users.noreply.github.com>
2021-11-11 10:52:55 +0530

    Update display.py

--- display.py
+++ display.py
@@ -1,2 +1,12 @@
 import cv2
 
+cap = cv2.VideoCapture(0)
+cap.set(3,1280)
+cap.set(4,720)
+
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    cv2.imshow("Display",img)
+    cv2.waitKey(100)
+    cv2.destroyAllWindows()
