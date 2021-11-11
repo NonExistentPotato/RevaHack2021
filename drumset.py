c58a33abfc8749209da40bfac4a4016eca168632
NonExistentPotato <93904995+NonExistentPotato@users.noreply.github.com>
2021-11-11 09:44:33 +0530

    contactless keyboard

--- drumset.py
+++
@@ -1,7 +0,0 @@
-import cv2
-img = cv2.imread("drumset.jpg")
-img_re = cv2.resize(img,(int(img.shape[0]*2),int(img.shape[1])))
-cv2.imshow("drums",img_re)
-key = cv2.waitKey(10000)
-#if key == ord('q'):
-cv2.destroyAllWindows()
