face1568f93294b2f1bf98654fc67627ae80f79c
NonExistentPotato <93904995+NonExistentPotato@users.noreply.github.com>
2021-11-11 12:17:53 +0530

    hand detector working

--- display.py
+++ display.py
@@ -1,11 +1,12 @@
 import cv2
 import mediapipe as mp
+import cvzone
 from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(0)
-cap.set(3,1280)
-cap.set(4,720)
-detector = HandDetector(detectionCon=0.8)
+cap.set(3, 1280)
+cap.set(4, 720)
+detector = HandDetector(detectionCon = 1, maxHands = 1)
 
 while True:
     success, img = cap.read()
