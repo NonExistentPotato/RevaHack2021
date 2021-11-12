77e917b7c4cf115b3d19224e8067d9e406700e52
NonExistentPotato <93904995+NonExistentPotato@users.noreply.github.com>
2021-11-12 21:47:41 +0530

    pin, amt screen done

--- display.py
+++ display.py
@@ -2,73 +2,111 @@ import cv2
 import mediapipe as mp
 import cvzone
 from cvzone.HandTrackingModule import HandDetector
-
-
-def landing_screen(hands, img):
-    img, bbox = cvzone.putTextRect(img,"Welcome To Non-Existent Bank",[100,200],2,3,offset=50,
-                 colorB=(255,255,255))
-    img, bbox_start= cvzone.putTextRect(img,"START",[350,500],1,2,offset=50, border=5,colorB=(255,255,255))
-
-    if hands:
-        lmList = hands[0]['lmList']
-        cursor = lmList[8]
-        length, info = detector.findDistance(lmList[8],lmList[12])
-        x1,x2,y1,y2 = bbox_start
-        if length < 30:
-            if x1< cursor[0] <x2 and y1< cursor[1] <y2:
-                cv2.rectangle(img, (x1,x2), (y1,y2), (255, 255, 255), cv2.FILLED)
-                print("clicked!")
-                return 0
-
-def draw_keypad(img):
-    img , bbox7 = cvzone.putTextRect(img,"7",[150,350],1,2,offset=40, colorB=(255,255,255))
-    img , bbox8 = cvzone.putTextRect(img,"8",[250,350],1,2,offset=40, colorB=(255,255,255))
-    img , bbox9 = cvzone.putTextRect(img,"9",[350,350],1,2,offset=40, colorB=(255,255,255))
-
-    img , bbox4 = cvzone.putTextRect(img,"4",[150,450],1,2,offset=40, colorB=(255,255,255))
-    img , bbox5 = cvzone.putTextRect(img,"5",[250,450],1,2,offset=40, colorB=(255,255,255))
-    img , bbox6 = cvzone.putTextRect(img,"6",[350,450],1,2,offset=40, colorB=(255,255,255))
-
-    img , bbox1 = cvzone.putTextRect(img,"1",[150,550],1,2,offset=40, colorB=(255,255,255))
-    img , bbox2 = cvzone.putTextRect(img,"2",[250,550],1,2,offset=40, colorB=(255,255,255))
-    img , bbox3 = cvzone.putTextRect(img,"3",[350,550],1,2,offset=40, colorB=(255,255,255))
-
-    img , bboxx = cvzone.putTextRect(img,"X",[150,650],1,2,offset=40, colorB=(255,255,255))
-    img , bbox0 = cvzone.putTextRect(img,"0",[250,650],1,2,offset=40, colorB=(255,255,255))
-    img , bboxe = cvzone.putTextRect(img,"<-",[350,650],1,2,offset=40, colorB=(255,255,255))
-
-    buttonList = [bboxe,bbox0,bbox1,bbox2,bbox3,bbox4,bbox5,bbox6,bbox7,bbox8,bbox9,bboxx]
-    return (img, buttonList)
+from time import sleep
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 detector = HandDetector(detectionCon = 1, maxHands = 1)
 
+keys = [['7','8','9'],['4','5','6'],['1','2','3'],['X','0','<-']]
+PINtext = "PIN: "
+AMTtext = "Amt: "
+
+def drawKeyboard(img, buttonList):
+    for button in buttonList:
+        x,y = button.pos
+        w,h = button.size
+        cv2.rectangle(img, button.pos, (x+w,y+h), (255,0,255), cv2.FILLED)
+        cv2.putText(img, button.text, (x+20,y+65), cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255),5)
+    return img
+
+class Button():
+    def __init__(self,pos,text,size=[85,85]):
+        self.pos = pos
+        self.text = text
+        self.size = size
+       
+buttonList = []
+for i in range(len(keys)):
+        for j,k in enumerate (keys[i]):
+            buttonList.append(Button([100 * j + 50, 100 * i +50], k))
+
+def landingScreen(img, PINtext):
+    cv2.rectangle(img, (50,200), (900,300), (255,0,255), cv2.FILLED)
+    cv2.putText(img, "Welcome to Non-Existent Bank", (80,260), cv2.FONT_HERSHEY_PLAIN, 3, (255,255,255),5)
+    cv2.rectangle(img, (350,400), (600,500), (255,0,255), cv2.FILLED)
+    cv2.putText(img, "START", (380,480), cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255),5)
+    if lmList:
+        l, _, _ = detector.findDistance(8,12,img, draw=False)
+        x,y = (350,400)
+        w,h = (600,500)
+        if x< lmList[8][0] <w and y< lmList[8][1] <h:
+            cv2.rectangle(img, (350,400), (600,500), (0,255,0), cv2.FILLED)
+            cv2.putText(img, "START", (380,480), cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255),5) 
+            if l < 30:
+                cv2.rectangle(img, (x,y), (w,h), (255, 255, 255), cv2.FILLED)
+                print("clicked!")
+                img , PINtext = pinScreen(img, PINtext)
+    return (img)
+
+def pinScreen(img, PINtext):
+    img = drawKeyboard(img,buttonList)
+
+    if lmList:
+        for button in buttonList:
+            x,y = button.pos
+            w,h = button.size
+            if x< lmList[8][0] <x+w and y< lmList[8][1] <y+h:
+                cv2.rectangle(img, button.pos, (x+w,y+h), (0,255,0), cv2.FILLED)
+                cv2.putText(img, button.text, (x+20,y+65), cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255),5)
+                l, _, _ = detector.findDistance(8,12,img, draw=False)
+                if l < 30:
+                    cv2.rectangle(img, button.pos, (x+w,y+h), (255,255,255), cv2.FILLED)
+                    cv2.putText(img, button.text, (x+20,y+65), cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255),5)
+                    if button.text == "<-":
+                        print("Next")
+                        img , AMTtext = amtScreen(img, AMTtext)
+                    else:
+                        PINtext += button.text
+                        sleep(2)
+    cv2.rectangle(img, (50,500), (400,600), (255,0,255), cv2.FILLED)
+    cv2.putText(img, PINtext, (80,580), cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255),5)
+    return (img, PINtext)
+
+def amtScreen(img, AMTtext):
+    img = drawKeyboard(img,buttonList)
+
+    if lmList:
+        for button in buttonList:
+            x,y = button.pos
+            w,h = button.size
+            if x< lmList[8][0] <x+w and y< lmList[8][1] <y+h:
+                cv2.rectangle(img, button.pos, (x+w,y+h), (0,255,0), cv2.FILLED)
+                cv2.putText(img, button.text, (x+20,y+65), cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255),5)
+                l, _, _ = detector.findDistance(8,12,img, draw=False)
+                if l < 30:
+                    cv2.rectangle(img, button.pos, (x+w,y+h), (255,255,255), cv2.FILLED)
+                    cv2.putText(img, button.text, (x+20,y+65), cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255),5)
+                    if button.text == "<-":
+                        print("Next")
+                    else:
+                        AMTtext += button.text
+                        sleep(1)
+    cv2.rectangle(img, (50,500), (400,600), (255,0,255), cv2.FILLED)
+    cv2.putText(img, AMTtext, (80,580), cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255),5)
+    return (img, AMTtext)
+
 while True:
     success, img = cap.read()
     img = cv2.flip(img, 1)
 
-    hands, img = detector.findHands(img, flipType=False)
+    img = detector.findHands(img)
+    lmList, bboxInfo = detector.findPosition(img)
 
-    #landing_screen(hands, img)
+    #img = landingScreen(img, PINtext)
+    img , AMTtext = amtScreen(img, AMTtext)
 
-    #lmList, bboxInfo = detector.findPosition(img)
-    img , bbox = cvzone.putTextRect(img,"Enter PIN",[100,200],1,2,offset=50,border=5, colorB=(255,255,255))
-    img , buttonList = draw_keypad(img)
-    if hands:
-        for button in buttonList:
-            x=button[0]
-            y=button[1]
-            w=button[2]
-            h=button[3]
-            lmList = hands[0]['lmList']
-            cursor = lmList[8]
-            length, info = detector.findDistance(lmList[8],lmList[12])
-            if length < 30:
-                if x< cursor[0] <x+w and y< cursor[1] <y+h :
-                    cv2.rectangle(img, (x,y), (x+w,y+h), (255, 255, 255), cv2.FILLED)
-                    print("clicked!")
 
 
     cv2.imshow("Display",img)
